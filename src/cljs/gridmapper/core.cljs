026a02cdf5608b025495eca9ac12c180339c35a6
Alex Schroeder <alex@gnu.org>
2014-03-26 08:44:46 +0100

    Add and remove doors using c2.dom.
    Issues remain with doors being added to the wrong group.

--- src/cljs/gridmapper/core.cljs
+++ src/cljs/gridmapper/core.cljs
@@ -3,8 +3,7 @@
   (:use [c2.core :only [unify]]
         [c2.event :only [on]])
   (:require [c2.scale :as scale]
-            [c2.dom :as dom]
-            [goog.events :as gevents]))
+            [c2.dom :as dom]))
 
 ;; --------------------------------------------------
 
@@ -14,19 +13,22 @@
 
 (defn make-grid
   ([dim]
-     (make-grid dim dim dim (set nil)))
+     (make-grid (- dim 1) (- dim 1) (- dim 1) (set nil)))
   ([x y maxy grid]
      (cond (> y 0)
-           (make-grid x (- y 1) maxy (conj grid (Cell. x y :empty)))
+           (make-grid x (- y 1) maxy (conj grid (Cell. x y :empty :empty :empty)))
            (> x 0)
-           (make-grid (- x 1) maxy maxy (conj grid (Cell. x y :empty)))
+           (make-grid (- x 1) maxy maxy (conj grid (Cell. x y :empty :empty :empty)))
            true
-           (conj grid (Cell. x y :empty)))))
+           (conj grid (Cell. x y :empty :empty :empty)))))
 
-(def model {:dim 30
-            :grid (atom (make-grid 30))
+(def model {:dim 5 ;; cells
+            :width 20 ;; pixels
+            :scale (scale/linear :domain [0 5]
+                                 :range [0 (* 5 20)])
+            :grid (atom (make-grid 5))
             :pen (atom nil)
-            :mode (atom nil)})
+            :mode (atom "door")})
 
 ;; --------------------------------------------------
 
@@ -34,45 +36,36 @@
 
 (bind! "#mapper"
        (let [dim (:dim model)
-             width 20
+             width (:width model)
              grid @(:grid model)
-             s (scale/linear :domain [0 dim]
-                             :range [0 (* dim width)])
+             s (:scale model)
              buttons [{:key "d" :pos 0 :id "door" :description "door"}
                       {:key "$" :pos 1 :id "secret" :description "secret door"}
                       {:key "t" :pos 2 :id "trap" :description "trap"}]]
          
-         [:div
+         [:div {:tabindex 1}
                      
           [:svg {:xmlns "http://www.w3.org/2000/svg"
-                 ;; :xmlns:xlink "http://www.w3.org/1999/xlink"
-                 ;; need some space to the right for the buttons
-                 :height (* dim width) :width (+ (* dim width) 200) }
+                 :width (+ (* dim width) 200) ;; add sidebar for buttons
+                 :height (* dim width)}
            
-           ;; [:defs
-           ;;  [:rect#empty {:x 0 :y 0 :height width :width width :fill "#555" :stroke "#000"}]
-           ;;  [:rect#floor {:x 0 :y 0 :height width :width width :fill "#fff" :stroke "#000"}]]
-
            [:g#grid
-            
             (unify grid
                    (fn [cell]
+                     [:rect {:x (s (:x cell)) :y (s (:y cell))
+                             :height width :width width :stroke "#000"
+                             :fill (if (= (:tile cell) :floor) "#fff" "#555")}]))]
 
-                     (cond (= (:tile cell) :floor)
-                           [:rect#floor {:x (s (:x cell)) :y (s (:y cell))
-                                         :height width :width width :fill "#fff" :stroke "#000"}]
-                           (= (:tile cell) :empty)
-                           [:rect#empty {:x (s (:x cell)) :y (s (:y cell))
-                                         :height width :width width :fill "#555" :stroke "#000"}])
-                     
-                     ;; [:use {:xlink:href (str "#" (:tile cell))
-                     ;;        :x (s (:x cell))
-                     ;;        :y (s (:y cell))}]
-
-                     ))]
+           [:g#doors
+            (unify grid
+                   (fn [cell]
+                     [:g ;; even cells without doors need this group
+                      (when (< (:y cell) (- dim 1))
+                        [:rect.south {:x (s (:x cell)) :y (s (+ (:y cell) 0.8))
+                                      :height (* width 0.4) :width width
+                                      :opacity 0.1}])]))]
 
            [:g#buttons
-            
             (unify buttons
                    (fn [{:keys [key pos id description]}]
                      [:g {:id id :class "button"}
@@ -100,8 +93,7 @@
                               :font-size 12
                               :font-family "Courrier"
                               :fill "#000"}
-                       description]]))]
-           ]]))
+                       description]]))]]]))
 
 ;; --------------------------------------------------
 
@@ -113,7 +105,7 @@
     (let [grid @(:grid model)]
       (reset! (:grid model)
               (conj (disj grid cell)
-                    (Cell. (:x cell) (:y cell) tile))))))
+                    (Cell. (:x cell) (:y cell) tile (:s-wall cell) (:e-wall cell)))))))
 
 (defn draw-on [cell]
   ;; use whatever the pen says
@@ -133,8 +125,49 @@
                 nil)))
 
 (defn set-mode [mode]
-  (p mode)
   (reset! (:mode model) mode))
+
+(defn set-south-wall [cell mode node]
+  (when mode
+    (let [grid @(:grid model)
+          width (:width model)
+          s (:scale model)
+          s-wall (if (= (:s-wall cell) :empty) mode :empty)]
+      (p (str "add " s-wall " to " (:x cell) "/" (:y cell)))
+      (reset! (:grid model)
+              (conj (disj grid cell)
+                    (Cell. (:x cell) (:y cell) (:tile cell) s-wall (:e-wall cell))))
+      (if (= s-wall :empty)
+        (dom/remove! node)
+        (dom/append! node 
+                     [:g.door
+                      [:rect.south {:x (s (:x cell)) :y (s (+ (:y cell) 0.8))
+                                    :height (* width 0.4) :width width
+                                    :fill "#000" :opacity 0.3 :stroke "#000"}]
+                      [:line.wall  {:x1 (s (:x cell)) :y1 (s (+ (:y cell) 1))
+                                    :x2 (s (+ (:x cell) 1)) :y2 (s (+ (:y cell) 1))
+                                    :stoke-width 4 :stroke "#000"}]
+                      [:rect.door  {:x (s (+ (:x cell) 0.2)) :y (s (+ (:y cell) 0.8))
+                                    :height (* width 0.4) :width (* width 0.6)
+                                    :fill-opacity 1 :fill "#fff"
+                                    :stoke-width 1 :stroke "#000"}]])))))
+
+(defn draw-south-wall [cell node]
+  (set-south-wall cell @(:mode model) node))
+          
+;; [:g
+;;  [:rect.south {:x (s (:x cell)) :y (s (+ (:y cell) 0.8))
+;;                :height (* width 0.4) :width width
+;;                :fill "#000" :opacity 0.3 :stroke "#000"}]
+;;  [:line.wall  {:x1 (s (:x cell)) :y1 (s (+ (:y cell) 1))
+;;                :x2 (s (+ (:x cell) 1)) :y2 (s (+ (:y cell) 1))
+;;                :stoke-width 4 :stroke "#000"}]
+;;  [:rect.door  {:x (s (+ (:x cell) 0.2)) :y (s (+ (:y cell) 0.8))
+;;                :height (* width 0.4) :width (* width 0.6)
+;;                :fill-opacity 1 :fill "#fff"
+;;                :stoke-width 1 :stroke "#000"}]]
+
+;; installing handlers
 
 (on "#grid" :mousedown
     (fn [cell]
@@ -145,6 +178,8 @@
     (fn [cell]
       ;; (p "up")
       (set-pen nil)))
+
+;; FIXME: add code to interpolate missed cells
 
 (on "#grid" :mousemove
     (fn [cell]
@@ -158,6 +193,16 @@
       (set-pen cell)
       (draw-on cell)
       (set-pen nil)))
+
+;; FIXME: keypresses still don't work
+
+(on "#grid" :keypress
+    (fn [cell node event]
+      (p event)))
+
+(on "#doors" :click
+    (fn [cell node]
+      (draw-south-wall cell node)))
 
 ;; buttons
 
