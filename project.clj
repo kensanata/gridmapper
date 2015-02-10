cbf4aab96bc672ffa2a5c74808e4bc73cddc0a01
Alex Schroeder <alex@gnu.org>
2015-02-10 11:36:46 +0100

    Deleted all the Clojure code. Moved SVG file into the root directory.

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject gridmapper "1.0.0"
-  :description "Utility to create simple dungeon maps for role-playing games."
-  :url "https://github.com/kensanata/gridmapper"
-  :license {:name "CC0 1.0 Universal"
-            :url "http://creativecommons.org/publicdomain/zero/1.0/"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main ^:skip-aot gridmapper.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
