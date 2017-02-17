c68af0aa95bbb73993176e7f0ce4f278f889e2d8
Alex Schroeder <alex@gnu.org>
2017-02-17 13:11:31 +0100

    Remove comments in init test

--- tests/init.js
+++ tests/init.js
@@ -3,10 +3,6 @@ module.exports = {
     browser
       .url('file://' + process.cwd() + '/gridmapper.svg')
       .waitForElementVisible('g#help', 1000)
-      // .setValue('input[type=text]', 'nightwatch')
-      // .waitForElementVisible('button[name=btnG]', 1000)
-      // .click('button[name=btnG]')
-      // .pause(1000)              
       .assert.containsText('#help', 'Alex Schroeder')
       .end();
   }
