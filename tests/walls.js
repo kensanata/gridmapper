8f95d639cf84471af4ef96c14e19539d3a61bfb1
Alex Schroeder <alex@gnu.org>
2017-02-17 14:20:04 +0100

    Rewrite test to be more robust
    Sadly I still cannot get the test to work for Firefox.

--- tests/walls.js
+++ tests/walls.js
@@ -2,19 +2,20 @@ module.exports = {
   'walls' : function (browser) {
     browser
       .url('file://' + process.cwd() + '/gridmapper.svg')
-      .assert.elementPresent('g#walls0')
+      .assert.elementPresent('#walls0')
       .keys('w')
-      .assert.elementPresent('#wall_0_0')
+      .waitForElementPresent('#wall_0_0', 1000)
       .assert.attributeEquals('#wall_0_0', 'rotate', '0')
       .keys('w')
       .assert.attributeEquals('#wall_0_0', 'rotate', '90')
       // enter wall mode
       .keys('mw')
+      .waitForElementPresent('#wall_1_0', 1000)
       .assert.attributeEquals('#wall_1_0', 'rotate', '0')
       // variant
       .keys('v')
+      .waitForElementPresent('#curtain_1_0', 1000)
       .assert.elementNotPresent('#wall_1_0')
-      .assert.elementPresent('#curtain_1_0')
       .end();
   }
 };
